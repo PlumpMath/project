5ce0b49c9b269e56363f18d098d1a592d0dc9fd6
xiechangqing <576535019@qq.com>
2016-09-09 16:57:57 +0800

    hahaha

--- yl/js/main.js
+++ yl/js/main.js
@@ -1,14 +1,17 @@
+/*service*/
 $(function(){
 	$('.type_btn li').click(function(){
 		$(this).addClass('on').siblings().removeClass('on');
 		var _index = $(this).index();
 		$('#type_con').find('.ser').eq(_index).slideDown().siblings().slideUp();
 	})
-})
+});
+/*service*/
 $(function(){
-	$('#about li').click(function(){
-		$(this).addClass('on').siblings().removeClass('on');
-		var _index = $(this).index();
-		$('#type_con').find('.main_li').eq(_index).slideDown().siblings().slideUp();
+	$('#aside').find('img').click(function(){
+			$('#scroll').show();
+	})
+		$('#close').click(function(){
+			$('#scroll').hide();
 	})
 })
